9c30f707a66cd51f47770596174e5051f8638f17
frauzola <meu.email@gmail.com>
2020-11-17 16:20:05 -0300

    exercicio de variaveis incompleta

---
+++ quinzena3/variaveis/Programa1.js
@@ -0,0 +1,4 @@
+let perguntas = {"Seu nome é Marco?", "Gosta de comer maça?", "Tem algum animal de estimação?"}
+let respostas = {true, true, false}
+
+console.log{perguntas, respostas}
---
+++ quinzena3/variaveis/js-vanilla-template/js-vanilla-template/src/index.js
@@ -0,0 +1,19 @@
+// primeiro exercicio, segue as respostas = Console.log (B), vai mostrar o numero 10 no console / Console.log (A,B), vai mostrar os numeros 10 e abaixo 5 no console //
+a = 10
+b = 10
+
+console.log (b)
+
+b = 5
+console.log (a,b)
+
+// segundo exercicio, segue respostas = console.log (a,b,c) , vai mostar "10 b 20 c a" todos na mesma linha do console.//
+
+a = 10
+b = 20
+c = a
+b = c
+a = b
+
+console.log (a,b,c)
+
