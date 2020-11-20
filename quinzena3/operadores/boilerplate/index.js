8106f2687aa59e7da596d200cd14314718d58238
frauzola <meu.email@gmail.com>
2020-11-19 21:47:05 -0300

    exercicio de operadores incomplesto

---
+++ quinzena3/operadores/boilerplate/index.js
@@ -0,0 +1,41 @@
+// Exercicio 1 resultado no console = a. false
+// Exercicio 1 resultado no console = b. false
+// Exercicio 1 resultado no console = c. true
+// Exercicio 1 resultado no console = e. number
+
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2
+
+// let resultado = bool1 && bool2
+// console.log("a. " , resultado)
+
+// resultado = bool1 && bool2 && boll3
+// console.log("b. " , resultado)
+
+// resultado = !resultado && (bool1 || bool1)
+// console.log("c. " , resultado)
+
+// console.log("e. " , typeof resultado)
+
+//exercicio 1
+// let idade = prompt("digite sua idade:")
+// let amigo = prompt("digite a idade do(a) seu(sua) melhor amig@:")
+
+// if (idade > amigo) {
+//     console.log("Sua idade é maior do que a do seu melhor amigo?" , true )
+// } else {console.log("Sua idade é maior do que a do seu melhor amigo?", false)}
+
+// confirm("Finalizado com sucesso")
+
+// exercicio 2
+// let par = prompt("digite um numero par:")
+// par %= 2
+// console.log(par)
+
+// se esta correto o meu codigo o resto aparece como 0 no console com numero par e numero impar aparece sempre 1
+
+// exercicio 4
+const nome = prompt("Digit seu nome: ")
+const email = prompt("Digite seu email: ")
+console.log( "O email " + email + " foi cadastrado com sucesso. Seja bem-vindo(a) " + nome)
