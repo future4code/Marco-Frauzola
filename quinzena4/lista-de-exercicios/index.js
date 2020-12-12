03c837d4ff7c781c89c7fc39f3f09e9a4ee496cf
frauzola <meu.email@gmail.com>
2020-12-12 16:10:09 -0300

    lista de exercicios

---
+++ quinzena4/lista-de-exercicios/index.js
@@ -0,0 +1,142 @@
+// Exercícios de interpretação de código =
+// Resposta 1 = No console irá aparecer o valor que coloquei no prompt * 100.
+// Resposta 2 = No console irá aparecer o valor de 165.
+// Resposta 3 = No console aparecerá a quantidade de numeros pares e a quantidade de impares dentro da array Numeros.
+// Resposta 4 = No console aparecerá o valor mais alto dentro da array Numeros, e o menor, que no caso é o unico valor negativo.
+
+/////////////////////////////////////////////////////////////////////////
+
+// Exercícios de Lógica de Programação =
+// Resposta 1 = forEach / map / filter
+// Resposta 2 = A - false / B - false / C - true / D - true / E - true
+// Resposta 3 =
+
+// var n = prompt("Digite um numero");
+// var total=n/2;
+// if(n/2 == 0){
+//     alert("Par");
+// }
+// if(n/2 == 1){
+//     alert("Impar");
+// }
+// alert(total);
+
+// resposta 4 =
+
+// function triangulo(a, b, c){
+
+//     var lado1 = a
+//     var lado2 = b
+//     var lado3 = c
+
+//     if (lado1 < (lado2+lado3) && lado2 < (lado1+lado3) && lado3 < (lado1+lado2)){
+//         if (lado1 == lado2 && lado2 = lado3){
+//             return "equilatero"
+//         }else if(lado1 == lado2 || lado1 == lado3 || lado3 == lado2){
+//             return "isosceles"
+//         }else {
+//             return "escaleno"
+//         }
+//     }
+// }
+
+//Resposta 5 =
+
+// var maior = 30
+// var menor = 15
+
+// if (maior > menor){
+//     return "O maior é " + maior
+// }
+
+/////////////////////////////////////////////////////////////////////////
+
+//Exercícios de Funções = 
+
+// var posicao = [1,2,3,4,5]
+
+// function numeros (numero){
+//     if (numero >= posicao.length) return "nada"
+
+//     return posicao[numero]
+// }
+
+// console.log("segundo maior é " + (4))
+// console.log("segundo menor é " + (2))
+
+// posicao(2,4)
+
+/////////////////////////////////////////////////////////////////////////
+
+//Exercícios de Objetos = 
+// Resposta do 1 = Usar objetos quando quiser que o nome dos elementos sejam strings / usar array quando quiser que os nomes dos elementos sejam numeros.
+
+// Resposta do 2=
+// const criaRetangulo = {
+//     lado1: 5,
+//     lado2: 4,
+//     perimetro: 2*(lado1 + lado2),
+//     area: (lado1*lado2)
+
+//     area: function() {
+//         return  "altura" + this.lado1 + "largura" + this.lado2 + "perimetro" + this.perimetro + "area" this.area 
+//     }
+
+// }
+
+// console.log(criaRetangulo.area())
+
+// Resposta do 3 = 
+
+// const filme = {
+//     titulo: "Gladiador",
+//     ano: 2000,
+//     diretor: "Ridley Scott",
+//     atores: ["Russell Crow" , "Joaquin Phoenix"]
+// }
+
+// const filmeTitulo = filme.titulo;
+// const filmeAno = filme.ano;
+// const filmeDiretor = filme.diretor;
+// const filmeAtor1 = "Russel Crow";
+// const filmeAtor2 = "Joaquin Phoenix"
+
+// console.log("venha assistir ao filme " + filmeTitulo + "do ano " + filmeAno + "dirigido por " + filmeDiretor + "e estrelado pelos atores " + filmeAtor1 + ", " + filmeAtor2 +)
+
+// Resposta do 4 = 
+
+// const anonimizarPessoas = {
+//     nome = "Marco"
+//     idade = 30
+//     email = "marco@gmail.com"
+//     endereco = "São Paulo"
+// } 
+
+// anonimizarPessoas.nome = 'Anonimo';
+// anonimizarPessoas.idade = 'Anonimo';
+// anonimizarPessoas.email = 'Anonimo';
+// anonimizarPessoas.endereco = 'Anonimo';
+
+// console.log(anonimizarPessoas)
+
+/////////////////////////////////////////////////////////////////////////
+
+//Exercícios de Funções de array
+// Resposta do 1 =
+
+// var pessoas = [
+// 	{ nome: "Pedro", idade: 20 },
+// 	{ nome: "João", idade: 10 },
+// 	{ nome: "Paula", idade: 12 },
+// 	{ nome: "Artur", idade: 89 } 
+// ]
+
+// function idade(pessoas) {
+//     if (idade => 20)
+//     return "adulto"
+// } 
+// return "crianças"
+
+// var resultado = idade(pessoas)
+// console.log(resultado)
+
